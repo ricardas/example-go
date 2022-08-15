d7487032b03bf4291f28a8fef14e59fb1c2a2a43
Ričardas Stoma <ricardas.stoma@gmail.com>
2022-08-15 18:28:42 +0000

    update

--- main/show_users/show_users.go
+++ main/show_users/show_users.go
@@ -4,7 +4,7 @@ import "github.com/ricardas/example-go/libs/user"
 
 func main() {
 	user1 := user.User{Name: "Maria", Id: 1}
-	user2 := user.User{Name: "Maria", Id: 1}
+	user2 := user.User{Name: "Alexa", Id: 2}
 
 	user1.Show()
 	user2.Show()
